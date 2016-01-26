928ad35e45676e84457ea8ae4e4b90c5d56773cf
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-01-26 17:41:48 +0100

    tests

---
+++ test/inc/tests.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const assert = require('chai').assert;
+const global = {};
+
+module.exports = function() {
+  it('save one', function() {
+    const doc = {a: 1, b: 2};
+    const res = this.silo.save(doc);
+    assert.equal(res.length, 1);
+    assert.property(res[0], '_id');
+    global._id = res[0]._id;
+  });
+
+  it('save many', function() {
+    const docs = [{b: 2, c: 3}, {c: 3, d: 4}, {d: 4, e: 5}];
+    const res = this.silo.save(docs);
+    assert.equal(res.length, docs.length);
+  });
+
+  it('find one', function() {
+    const res = this.silo.find(global._id);
+    assert.equal(res.length, 1);
+  });
+
+  it('find many', function() {
+    const res = this.silo.find({c: 3});
+    assert.equal(res.length, 2);
+  });
+};
