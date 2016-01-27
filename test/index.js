844734b374a5ad01be87d445f8f30ca73fee1a62
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-01-27 14:04:00 +0100

    eslint

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ const tests = require('./inc/tests');
 const Silo = require('../lib');
 
 ['memory', 'diskdb'].forEach(function(provider) {
-  describe(provider + ' provider', function () {
+  describe(provider + ' provider', function() {
     before(function() {
       this.silo = new Silo(provider);
       this.silo.remove();
